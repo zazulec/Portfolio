e62110970c2d31ab3f1b51fd919ddeb7acf31b79
Paweł Zarzycki <pawelzarzycki@MacBook-Air-Pawe.local>
2021-01-25 21:38:38 +0100

    fix: updated src to all photos

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,21 +3,42 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-
   @Input() projects: Array<Object> = [
-    {source:"../../../assets/photo/weatherApp.png", title: "weatherApp", githubLink:'https://github.com/zazulec/Weather-react-app', liLink: "https://zazulec.github.io/Weather-react-app/#/"},
-    {source:"../../../assets/photo/landingPageH.png", title: "landingPageH", githubLink:'https://github.com/zazulec/LandingPage_H', liLink: "https://zazulec.github.io/LandingPage_H/"},
-    {source:"../../../assets/photo/gowShop.png", title: "gowShop", githubLink:'https://github.com/zazulec/gwo-shop', },
-    {source:"../../../assets/photo/calendar.png", title: "calendar", githubLink:'https://github.com/zazulec/calendar', },
-    {source:"../../../assets/photo/oldPortfolio.png", title: "oldPortfolio", githubLink:'https://github.com/zazulec/Pawel-Zarzycki', liLink: "https://zazulec.github.io/Pawel-Zarzycki/"}
-]
-  
+    {
+      source: 'assets/photo/weatherApp.png',
+      title: 'weatherApp',
+      githubLink: 'https://github.com/zazulec/Weather-react-app',
+      liLink: 'https://zazulec.github.io/Weather-react-app/#/',
+    },
+    {
+      source: 'assets/photo/landingPageH.png',
+      title: 'landingPageH',
+      githubLink: 'https://github.com/zazulec/LandingPage_H',
+      liLink: 'https://zazulec.github.io/LandingPage_H/',
+    },
+    {
+      source: 'assets/photo/gowShop.png',
+      title: 'gowShop',
+      githubLink: 'https://github.com/zazulec/gwo-shop',
+    },
+    {
+      source: 'assets/photo/calendar.png',
+      title: 'calendar',
+      githubLink: 'https://github.com/zazulec/calendar',
+    },
+    {
+      source: 'assets/photo/oldPortfolio.png',
+      title: 'oldPortfolio',
+      githubLink: 'https://github.com/zazulec/Pawel-Zarzycki',
+      liLink: 'https://zazulec.github.io/Pawel-Zarzycki/',
+    },
+  ];
+
   constructor() {}
 
-  ngOnInit(): void {
-  }
-  changeVisible = () => alert("hover")
+  ngOnInit(): void {}
+  changeVisible = () => alert('hover');
 }
--- src/app/pages/tech/tech.component.ts
+++ src/app/pages/tech/tech.component.ts
@@ -1,42 +1,30 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tech',
   templateUrl: './tech.component.html',
   styleUrls: ['./tech.component.scss'],
-  animations: [
-    // animation triggers go here
-  ],
 })
 export class TechComponent implements OnInit {
   @Input() techLogoData: Array<Object> = [
-    { img: '../../../assets/logo/javaSctiptLogo.svg', title: 'JavaScript' },
-    { img: '../../../assets/logo/typeScriptLogo.png', title: 'TypeScript' },
-    { img: '../../../assets/logo/reactLogo.webp', title: 'React' },
-    { img: '../../../assets/logo/reduxLogo.png', title: 'Redux' },
-    { img: '../../../assets/logo/angularLogo.png', title: 'Angular' },
-    { img: '../../../assets/logo/bootstrapLogo.png', title: 'Bootstrap' },
-    { img: '../../../assets/logo/materialUiLogo.png', title: 'Material-Ui' },
+    { img: 'assets/logo/javaSctiptLogo.svg', title: 'JavaScript' },
+    { img: 'assets/logo/typeScriptLogo.png', title: 'TypeScript' },
+    { img: 'assets/logo/reactLogo.webp', title: 'React' },
+    { img: 'assets/logo/reduxLogo.png', title: 'Redux' },
+    { img: 'assets/logo/angularLogo.png', title: 'Angular' },
+    { img: 'assets/logo/bootstrapLogo.png', title: 'Bootstrap' },
+    { img: 'assets/logo/materialUiLogo.png', title: 'Material-Ui' },
     {
-      img: '../../../assets/logo/styledComponentsLogo.png',
+      img: 'assets/logo/styledComponentsLogo.png',
       title: 'Styled',
     },
-    { img: '../../../assets/logo/githubLogo.png', title: 'Github' },
-    { img: '../../../assets/logo/adobeXdLogo.png', title: 'adobeXD' },
-    { img: '../../../assets/logo/figmaLogo.png', title: 'Figma' },
-    { img: '../../../assets/logo/postmanLogo.png', title: 'Postman' },
+    { img: 'assets/logo/githubLogo.png', title: 'Github' },
+    { img: 'assets/logo/adobeXdLogo.png', title: 'adobeXD' },
+    { img: 'assets/logo/figmaLogo.png', title: 'Figma' },
+    { img: 'assets/logo/postmanLogo.png', title: 'Postman' },
   ];
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
